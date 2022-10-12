f1b3bd6e7430d33dc0a95806d472e958b774e972
Neosake <70122910+Neosake@users.noreply.github.com>
2022-10-12 16:53:13 -0500

    Create main.hs

---
+++ main.hs
@@ -0,0 +1,10 @@
+{- Import Required Modules. -}
+
+{- Declare Necessary Functions. -}
+main :: IO ()
+
+{- Declare Necessary Binding's. -}
+
+{- Environment For Execution. -}
+main = do
+   print "Satisfies HLS errors."
